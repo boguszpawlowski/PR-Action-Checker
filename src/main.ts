a8b029e41a80a915110aa4903ce863816952e4cc
boguszpawlowski <bogusz.pawlowski@gmail.com>
2022-12-21 22:53:07 +0100

    prod dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,13 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as github from '@actions/github'
 
-async function run(): Promise<void> {
+function run(): void {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    const prBody = github.context.payload.pull_request?.body
+    if (prBody?.includes("[ ]") === false) {
+      core.setFailed("Not all the tasks have been completed")
+    }
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
